edd0016890c9abb254b2d0c73db53a8ccb1bc0cd
Stepan Kryukov <stephenkryukov@gmail.com>
2022-07-22 00:13:30 +0300

    renderMarkup + gallery styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import { FetchFilms } from "./js/FetchFilmsClass";
-
+import { FetchFilms } from './js/FetchFilmsClass';
+import './js/renderMarkup';
 const a = 1;
-
-
 
 const fetchFilms = new FetchFilms();
