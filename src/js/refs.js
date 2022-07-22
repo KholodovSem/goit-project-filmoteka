e3ec91043d3ccd6a59ca7e47769f3ff92456dacb
Pablo Escobar <pabloescobaro@drag.com>
2022-07-22 18:46:37 +0300

    Refactoring the registration/login modal window

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,4 +1,20 @@
 export const refs = {
   movieContainer: document.querySelector('.films__gallery'),
   backToTopBtn: document.querySelector('.back-to-top-btn'),
+  registrationModal: {
+    regModal: document.querySelector('.registration-modal'),
+    closeButtons: {
+      userAccBtnRef: document.querySelector('.registration-btn'),
+      topCloseRegModalBtnRef: document.querySelector('.modal-btn'),
+      bottomCloseRegModalBtnRef: document.querySelector(
+        '.tab-change-btn.cancel'
+      ),
+    },
+    tabs: {
+      signInTabRef: document.querySelector('[data-sign-in-tab]'),
+      newAccTabRef: document.querySelector('[data-new-account-tab]'),
+      newAccBoolean: false,
+      signInBoolean: false,
+    },
+  },
 };
